edd190406edcfdcf35350fc44fd56076dda47357
zizitmd07 <zizitmd07@gmail.com>
2026-01-06 03:40:51 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'unbelievable-filter-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js'
+];
+
+// 설치 이벤트
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('캐시 열기');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('오래된 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch 이벤트 (네트워크 우선, 실패 시 캐시 사용)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // 응답 복제
+        const responseToCache = response.clone();
+        
+        // 캐시 업데이트
+        caches.open(CACHE_NAME)
+          .then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        
+        return response;
+      })
+      .catch(() => {
+        // 네트워크 실패 시 캐시에서 가져오기
+        return caches.match(event.request);
+      })
+  );
+});
